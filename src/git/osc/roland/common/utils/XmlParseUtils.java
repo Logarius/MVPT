90df51254af4bc03f6ea8c919fcf4df4bb3ad169
coy <coy20150428@gmail.com>
2017-05-24 16:04:14 +0800

    添加了SAX解析XML的功能

---
+++ src/git/osc/roland/common/utils/XmlParseUtils.java
@@ -0,0 +1,33 @@
+package git.osc.roland.common.utils;
+
+import git.osc.roland.common.utils.xmlParseHandler.UserParseHandler;
+
+import git.osc.roland.domain.User;
+import org.xml.sax.SAXException;
+
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+/**
+ * Created by coy on 2017/5/24.
+ */
+public class XmlParseUtils {
+    public static List<User> getUsers() throws ParserConfigurationException, SAXException, IOException {
+        // 加载文件返回文件的输入流
+        InputStream is = XmlParseUtils.class.getClassLoader().getResourceAsStream("users.xml");
+        UserParseHandler handler = new UserParseHandler();
+        // 1. 得到SAX解析工厂
+        SAXParserFactory saxParserFactory = SAXParserFactory.newInstance();
+        // 2. 让工厂生产一个sax解析器
+        SAXParser newSAXParser = saxParserFactory.newSAXParser();
+        // 3. 传入输入流和handler，解析
+        newSAXParser.parse(is, handler);
+        is.close();
+        return handler.getUsers();
+    }
+}
--- src/git/osc/roland/main/Boot.java
+++ src/git/osc/roland/main/Boot.java
@@ -1,6 +1,9 @@
 package git.osc.roland.main;
 
+import git.osc.roland.common.utils.XmlParseUtils;
+
 import javax.swing.*;
+
 
 public class Boot {
 	
@@ -13,6 +16,18 @@ public class Boot {
         // 添加 "Hello World" 标签
         JLabel label = new JLabel("Hello World");
         frame.getContentPane().add(label);
+        try {
+            JTextArea commentTextArea = new JTextArea(XmlParseUtils.getUsers().toString());
+            JScrollPane scrollPane = new JScrollPane(commentTextArea);
+            JPanel controlPanel = new JPanel();
+            controlPanel.add(scrollPane);
+            frame.getContentPane().add(controlPanel);
+
+        }catch (Exception e){
+            //...Todo
+        }
+
+
 
         // 显示窗口
         frame.setVisible(true);
@@ -20,7 +35,7 @@ public class Boot {
 	
 	public static void main(String[] args) {
 		javax.swing.SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
+            public void run()  {
             	new Boot().run();
             }
         });
